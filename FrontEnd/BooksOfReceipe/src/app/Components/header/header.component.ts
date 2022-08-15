59650e08a9348ea0e5851e1b01e6bdbd5dd64bbe
Dudin Alexey <dudin.lesha@gmail.com>
2022-08-15 20:32:54 +0300

    - add login (not work)

--- FrontEnd/BooksOfReceipe/src/app/Components/header/header.component.ts
+++ FrontEnd/BooksOfReceipe/src/app/Components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {MatDialog, MatDialogRef} from '@angular/material/dialog';
+import { LoginComponent } from '../login/login.component';
 
 @Component({
   selector: 'app-header',
@@ -10,9 +12,12 @@ export class HeaderComponent implements OnInit {
   logoSrc = "./assets/images/Recipes.svg";
   loginSrc = "./assets/images/login.svg";
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
   
   ngOnInit(): void {
   }
-
+  
+  openDialog() {
+    this.dialog.open(LoginComponent);
+  }
 }
