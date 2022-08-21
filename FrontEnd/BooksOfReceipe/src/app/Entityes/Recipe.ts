f14eb8651d8a97d8c450e4296e3e6558c1ca446c
Dudin Alexey <dudin.lesha@gmail.com>
2022-08-21 14:23:02 +0300

    - add expanded smart info

--- FrontEnd/BooksOfReceipe/src/app/Entityes/Recipe.ts
+++ FrontEnd/BooksOfReceipe/src/app/Entityes/Recipe.ts
@@ -1,7 +1,14 @@
+import { TagClass } from "./TagClass";
+
 export class Recipe {
     title: string = "";
     text: string = "";
     imagePath: string = "";
     likeCount: number = 0;
     timer: number = 0;
+    persons: number = 0;
+    tags: TagClass [] = [
+        {
+            text : ""
+        }];
     }
