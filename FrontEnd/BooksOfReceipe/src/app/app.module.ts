2a34e69a27c8aae77f306f7d2a5778167d155820
Dudin Alexey <dudin.lesha@gmail.com>
2022-08-24 07:13:25 +0300

    - add step module

--- FrontEnd/BooksOfReceipe/src/app/app.module.ts
+++ FrontEnd/BooksOfReceipe/src/app/app.module.ts
@@ -25,6 +25,7 @@ import {MAT_FORM_FIELD_DEFAULT_OPTIONS} from '@angular/material/form-field';
 import { RecipesComponent } from './Pages/recipes/recipes.component';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { ExpandedSmartInfoCardComponent } from './Components/expanded-smart-info-card/expanded-smart-info-card.component';
+import { StepComponent } from './Components/step/step.component';
 
 @NgModule({
   declarations: [
@@ -41,7 +42,8 @@ import { ExpandedSmartInfoCardComponent } from './Components/expanded-smart-info
     LoginComponent,
     RegistrationComponent,
     RecipesComponent,
-    ExpandedSmartInfoCardComponent
+    ExpandedSmartInfoCardComponent,
+    StepComponent
   ],
   imports: [
     BrowserModule,
