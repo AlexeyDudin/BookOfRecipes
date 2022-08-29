ada2508f7b6ca38ac0584196ad70f98a3c9e104b
Dudin Alexey <dudin.lesha@gmail.com>
2022-08-29 21:18:56 +0300

    - add recype services

---
+++ FrontEnd/BooksOfReceipe/src/app/Services/resipe.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { User } from '../Entityes/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ResipeService {
+
+  constructor() { }
+
+  getRecypeById(id: number) {
+
+  }
+
+  getUserRecypes(user: User) {
+    
+  }
+}
