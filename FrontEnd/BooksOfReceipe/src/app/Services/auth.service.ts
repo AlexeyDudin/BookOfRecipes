e037003b21f5de943ff07ce64c6849b06d018337
Dudin Alexey <dudin.lesha@gmail.com>
2022-08-30 12:49:00 +0300

    - fix service working - fix user entity

--- FrontEnd/BooksOfReceipe/src/app/Services/auth.service.ts
+++ FrontEnd/BooksOfReceipe/src/app/Services/auth.service.ts
@@ -1,21 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { User } from '../Entityes/user';
+import { BaseService } from './base.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private baseService: BaseService) { }
 
   authorize(){
 
   }
 
   createUser(user: User) {
-    let result;
-    let postResult = this.http.post('http://localhost:5119/api/user/add', user, {responseType: "json"}).subscribe(data => {result = data});
-    return result;
+    return this.baseService.sendPost('/api/user/add', user);
   }
 }
---
+++ Infrastructure/Migrations/20220830051952_FixLoginUnique.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    public partial class FixLoginUnique : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Login",
+                table: "User",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_Login",
+                table: "User",
+                column: "Login",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_User_Login",
+                table: "User");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Login",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+    }
+}
